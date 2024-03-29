953b2e3460586da719d833f51448bae98f6cbad9
CHOHUNE <yuureru@gmail.com>
2024-03-29 13:03:58 +0900

    git ignore setting

---
+++ src/main/java/com/example/springoauth2jwt/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.springoauth2jwt.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+
+    public String mainAPI() {
+
+        return "myroute";
+    }
+}
