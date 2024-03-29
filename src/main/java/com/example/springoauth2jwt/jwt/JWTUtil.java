ca69904dc226087da51ca4b1d78ef6f3b8e149be
CHOHUNE <yuureru@gmail.com>
2024-03-29 16:45:47 +0900

    JWTUtil 발급 및 검증 클래스

---
+++ src/main/java/com/example/springoauth2jwt/jwt/JWTUtil.java
@@ -0,0 +1,54 @@
+package com.example.springoauth2jwt.jwt;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("roloe", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
+
+//Jwts.parser(): JWT 토큰을 파싱하기 위한 Parser 생성
+//- verifyWith(): 생성한 Parser에 검증에 필요한 Key 제공
+//- build(): Parser 구성 완료
+//- parseClaims(): 주어진 토큰 파싱
+//parser() 부터 parseClaims()까지 동일
+//- getPayload(): 토큰 Payload 영역 접근
+//- getExpiration(): 토큰의 Expiration Claim 값 얻기
+//- before(): Expiration 값이 현재 시간 이전인지 확인
