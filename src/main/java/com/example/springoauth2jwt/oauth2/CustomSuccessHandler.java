0b20c03ada41e37c62e70079bc7c4a0a505631ef
CHOHUNE <yuureru@gmail.com>
2024-06-11 18:49:26 +0900

    FIX : ETC

--- src/main/java/com/example/springoauth2jwt/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/example/springoauth2jwt/oauth2/CustomSuccessHandler.java
@@ -29,7 +29,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         String username = customUserDetails.getUsername();
 
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authentication.getAuthorities().iterator();
 
         GrantedAuthority authority = iterator.next();
@@ -45,7 +45,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private Cookie createCookie(String key, String value) {
         Cookie cookie =new Cookie(key, value);
         cookie.setMaxAge(60 * 60 * 60);
-        //coockike.setSecure(true);
+        //cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
 
